43906d97c436a115e5f6d9a3b0b68a93693dcd34
Timothy Moores <info@tmoor.es>
2021-06-23 22:13:01 +0100

    remove raw verilog

--- harvard/rtl/mips_cpu_4harvard_writeback.v
+++
@@ -1,6 +0,0 @@
-`timescale 1ps/1ps
-module harvard_writeback (
-  
-);
-
-endmodule
--- harvard/test/test_mips_cpu_harvard.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf test/build
-mkdir -p test/build
-gcc test/test.c -o test/build/test.bin
-test/build/test.bin $@
